11567465523e05e19e9dfb940e8b9f609fbc93df
boikomykola10 <koliaboiko10@gmail.com>
2021-03-29 23:15:14 +0300

    task_03

--- lesson_2/task_3.py
+++ lesson_2/task_3.py
@@ -1,4 +1,4 @@
-number = int(input())
+number = int(input('Введите трёхзначное число: '))
 n1 = number // 100  # первая цифра
 n2 = number // 10 % 10  # вторая цифра
 n3 = number % 10    # последняя цифра
