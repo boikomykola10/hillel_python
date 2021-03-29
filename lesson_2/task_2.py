91b954f3ddde847d1804edca5d6823e0d595b36d
boikomykola10 <koliaboiko10@gmail.com>
2021-03-29 23:28:39 +0300

    task_02

--- lesson_2/task_2.py
+++ lesson_2/task_2.py
@@ -1,5 +1,7 @@
 class1 = int(input())
 class2 = int(input())
 class3 = int(input())
-allpupils = class1 + class2 + class3
-print(allpupils // 2 + allpupils % 2)
+deskforclass1 = class1 // 2 + class1 % 2
+deskforclass2 = class2 // 2 + class2 % 2
+deskforclass3 = class3 // 2 + class3 % 2
+print(deskforclass1 + deskforclass2 + deskforclass3)
