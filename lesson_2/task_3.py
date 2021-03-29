f89a52df45e66c48f81035d0fbd16027d191af70
boikomykola10 <81511227+boikomykola10@users.noreply.github.com>
2021-03-29 22:50:42 +0300

    Update task_3.py

--- lesson_2/task_3.py
+++ lesson_2/task_3.py
@@ -1,6 +1,6 @@
 number = int(input())
 n1 = number // 100  # первая цифра
 n2 = number // 10 % 10  # вторая цифра
-n3 = number % 10    # последняя цыфра
+n3 = number % 10    # последняя цифра
 print(str(n3) + str(n2) + str(n1))
 
