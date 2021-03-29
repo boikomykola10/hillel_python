63e93b731cdd9ac6c4c1506105916a465a8b1e94
boikomykola10 <koliaboiko10@gmail.com>
2021-03-29 23:12:18 +0300

    task_03

--- lesson_2/task_3.py
+++ lesson_2/task_3.py
@@ -2,5 +2,4 @@ number = int(input())
 n1 = number // 100  # первая цифра
 n2 = number // 10 % 10  # вторая цифра
 n3 = number % 10    # последняя цифра
-print(str(n3) + str(n2) + str(n1))
-
+print(n3, n2, n1, sep='')
