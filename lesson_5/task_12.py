cbb992f42dfa33d3d8f70211aa48a40d4fe21614
boikomykola10 <koliaboiko10@gmail.com>
2021-04-09 21:57:40 +0300

    task_12

--- lesson_5/task_12.py
+++ lesson_5/task_12.py
@@ -4,6 +4,6 @@ lst = [random.randint(1,20) for _ in range(15)]
 print(lst)
 count = 0
 for i in range(1, len(lst)-1):
-    if lst[i - 1] < lst[i] < lst[i + 1]:
+    if lst[i] > lst[i - 1] and lst[i] > lst[i + 1]:
         count += 1
 print(count)
--- lesson_5/task_14.py
+++ lesson_5/task_14.py
@@ -3,7 +3,7 @@ import random
 lst = [random.randrange(1, 20, 1) for _ in range(15)]
 print(lst)
 for i in range(5, len(lst)):
-    lst.insert(0, lst[i])
+    lst[0] = lst[len(lst)-1]
 
 print(lst)
 
