1f7965788650c63c55b6bb8f9637a160f341a157
boikomykola10 <koliaboiko10@gmail.com>
2021-04-10 14:15:14 +0300

    task_14

--- lesson_5/task_14.py
+++ lesson_5/task_14.py
@@ -2,9 +2,10 @@ import random
 
 lst = [random.randrange(1, 20, 1) for _ in range(15)]
 print(lst)
-for i in range(5, len(lst)):
-    lst[0] = lst[len(lst)-1]
-
+k = int(input('Введите индекс элемента: '))
+for i in range(k, len(lst)-1):
+    lst[i] = lst[i+1]
+lst.pop()
 print(lst)
 
 
