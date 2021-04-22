2ef184e7c8b22a16dc14811a6b7323fa1f21d27d
boikomykola10 <koliaboiko10@gmail.com>
2021-04-22 20:11:02 +0300

    task_23

---
+++ lesson_9/task_23.py
@@ -0,0 +1,11 @@
+y = int(input('Введите год: '))
+
+
+def is_year_leap(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+
+
+print(is_year_leap(y))
