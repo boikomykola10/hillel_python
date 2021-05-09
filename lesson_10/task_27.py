5c7bd8f5d75eb9cee36d57bef859d0b9235c830e
boikomykola10 <koliaboiko10@gmail.com>
2021-05-09 12:54:26 +0300

    task_27

--- lesson_10/task_27.py
+++ lesson_10/task_27.py
@@ -5,18 +5,19 @@ from random import randint
 m = int(input('Введите число столбцов: '))
 n = int(input('Введите число строк: '))
 
-lst = [[randint(10, 50) for x in range(n)] for i in range(m)]
-for i in lst:
-    s = 0
-    for j in i:
-        print('{:>4}'.format(j), end='')
-        s += j
-    print('{:>8}'.format(s), end='')
-    print()
-print()
+lst = [[randint(10, 50) for _ in range(m)] for _ in range(n)]
+ext_lst = [0] * m
 for i in range(n):
     s = 0
     for j in range(m):
-        s += lst[j][i]
-    print('{:>4}'.format(s), end='')
+        print('{:>4}'.format(lst[i][j]), end='')
+        s += lst[i][j]
+        ext_lst[j] += lst[i][j]
+    print('{:>8}'.format(s), end='')
+    print()
+print()
+
+for i in range(m):
+    print('{:>4}'.format(ext_lst[i]), end='')
+
 
